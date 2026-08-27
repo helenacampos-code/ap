018347ba7e6f4502d6b69fa64270e06c7549b5b4
helena <brasilhelena3@gmail.com>
2026-08-27 16:01:32 -0300

    2.0

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 app = FastAPI()
-@app.get("/")
-def ler_raiz():
-    return{"mensage": "Minha primeira API está funcionando!"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class itens (BaseModel):
+    lote: int
+    item: int
+    nome: str
+    descritivo: str
+    unid_de_medida: str
+    quant_total: int
+
+@app.post ("/enviar-json")
+async def receber_dados(dados: itens):
+    return {
+        "status": "Sucesso", 
+        "mensagem": f"JSON recebido: {itens.nome}
+    }
+
+    
